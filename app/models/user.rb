a43343db934138a2295408d956e157c898c51e5c
Dmitry Ovcharenko <ovcharenkodmitry@gmail.com>
2013-06-07 17:36:40 +0400

    Изъят параметр registerable из devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
+  # :token_authenticatable, :confirmable, :registerable 
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, 
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :username,:email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
 
-  validates_presence_of :username, :message => 'Username cannot be blank'
+  # validates_presence_of :username, :message => 'Username cannot be blank'
 end
